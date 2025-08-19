a4f2e9f7c19ba05147d23378bbecc7d80f411043
Deep Kumbhare <kumbharedeep85@gmail.com>
2025-08-19 13:13:37 +0530

    refactor(svelte-kit-example): added tailwindcss for svelte-kit-example for testing.

--- examples/svelte-kit-example/src/routes/+page.svelte
+++ examples/svelte-kit-example/src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <main>
-  <h1>Welcome to SvelteKit</h1>
+  <h1 class="text-red-600 dark:text-blue-500">Welcome to SvelteKit</h1>
   <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 </main>
